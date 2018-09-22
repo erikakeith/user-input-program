c32150e7dc8789e5e90aed638a2d1441c7082bd1
Erika Keith <erikakeith13@gmail.com>
2018-09-22 15:59:18 +0000

    firtst commit

---
+++ user_input_statistics.rb
@@ -0,0 +1,23 @@
+def main
+
+  # we need an array to store our data
+['Hello', 'Goodbye']
+  # how do we assign an array to a variable?
+ sharks = ["Hammerhead", "Great White", "Tiger"]
+  loop do
+
+    # continue to ask for a number
+
+    # challenge: how do I check that may input is an integer and not a string?
+
+    break if value == '' # this will end the loop if the user input is blank
+
+  end
+
+  # output data
+
+end
+
+
+
+main if __FILE__ == $PROGRAM_NAME
